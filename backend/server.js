bdd715c43ea8a57a34dca768e9d490448991ae0c
Alok mishra <alokm5999@gmail.com>
2026-04-04 22:32:54 +0530

    fix cors error

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://ai-chatbot-psi-olive.vercel.app"
+  ]
 }));
 
 app.use(express.json());
